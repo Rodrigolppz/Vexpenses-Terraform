19fd4ec9c229b6eb6554f1781b2953db608616bc
Rodrigo Ascenção <157632873+Rodrigolppz@users.noreply.github.com>
2024-10-19 13:29:31 -0300

    Create keypairs.tf

---
+++ Arquivos/keypairs/keypairs.tf
@@ -0,0 +1,11 @@
+# Configurações das keypairs
+
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "${var.projeto}-${var.candidato}-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
