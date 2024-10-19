295c8e01a95e470ea0080f5ffeb78fa993df37c0
Rodrigo Ascenção <157632873+Rodrigolppz@users.noreply.github.com>
2024-10-19 13:34:57 -0300

    Create outputs.tf

---
+++ Arquivos/outputs/outputs.tf
@@ -0,0 +1,12 @@
+# Definições de saídas
+
+output "private_key" {
+  description = "Chave privada para acessar a instância EC2"
+  value       = tls_private_key.ec2_key.private_key_pem
+  sensitive   = true
+}
+
+output "ec2_public_ip" {
+  description = "Endereço IP público da instância EC2"
+  value       = aws_instance.debian_ec2.public_ip
+}
