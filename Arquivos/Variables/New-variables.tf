462515b684bcdd2c0098c441df97b1282873645b
Rodrigo Ascenção <157632873+Rodrigolppz@users.noreply.github.com>
2024-10-19 09:01:28 -0300

    Create New-variables.tf

---
+++ Arquivos/Variables/New-variables.tf
@@ -0,0 +1,11 @@
+variable "projeto" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "VEXPENSES" # Mantive o mesmo nome
+}
+
+variable "candidato" {
+  description = "Nome do candidato"
+  type        = string
+  default     = "Rodrigo Ascenção" # Substituir pelo meu nome
+}
